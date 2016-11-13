c921946e05cb61bee101090502eb0aa75a6aa2ca
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2016-11-12 18:13:47 -0600

    Added link to documentation.

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ SDGLogic extends support for Boolean logic.
 ## SDGLogic provides:
 
 * Operators: ≠, ¬, ∧, ∨, etc.
+
+## Documentation:
+
+Complete API documentation is available [here](https://sdggiesbrecht.github.io/SDGLogic/).
--- Tests/SDGLogicTests/SDGLogicTests.swift
+++ Tests/SDGLogicTests/SDGLogicTests.swift
@@ -4,6 +4,8 @@
 //
 // Copyright ©2016 Jeremy David Giesbrecht and the SDGLogic contributors.
 //
+// Soli Deo gloria
+//
 // Licensed under the Apache License, Version 2.0
 // See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
 
